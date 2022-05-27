f05ec524356c0e58c7c2eebc7e2f450fdcce37bc
Michael Rotteveel <michael_rotteveel@hotmail.nl>
2022-05-27 14:12:51 +0200

    Renamed to Mike's Minde

--- frontend-next-js/pages/index.tsx
+++ frontend-next-js/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">The Adventures Of MiRo!</a>
+          Welcome to <a href="https://nextjs.org">Mike's Mind!</a>
         </h1>
 
         <p className={styles.description}>
